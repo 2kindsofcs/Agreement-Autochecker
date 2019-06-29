27a12a17bca9c212cf814ef17e105fb7d933be15
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-06-29 23:37:39 +0900

    Uncheck agreement to ads

--- contentScripts.js
+++ contentScripts.js
@@ -1,19 +1,56 @@
-let pageTitle = document.getElementsByTagName("title")[0].innerText;
-if (pageTitle.includes("본인확인")) {
-    let inputList = document.getElementsByTagName("input");
-    document.querySelector
-    for (let i=0; i<inputList.length; i++) {
-        if (inputList[i].type === "checkbox") {
-            inputList[i].checked = true;
-        }
+const autoCheck = function() {
+  const pageTitle = document.title;
+  const inputList = document.getElementsByTagName('input');
+  const crit1 = pageTitle.includes('본인') && pageTitle.includes('확인');
+  const crit2 = pageTitle.includes('휴대폰') && pageTitle.includes('인증');
+  if (crit1 || crit2) {
+    for (let i = 0; i < inputList.length; i++) {
+      if (inputList[i].type === 'checkbox') {
+        inputList[i].checked = true;
+      }
     }
-}
-
-
-
-//     for (let i=firstBoxIndex; i<firstBoxIndex+4; i++) {
-//         inputList[i].checked = true;
-//     }
-// }
-
-    // document.querySelector(`label[for=${$0.name}]`).textContent.search("선택") != -1
+  }
+};
+/**
+ * @noreturn
+ */
+(async function getCheckDone() {
+  try {
+    if (document.title) {
+      let ifChange = false;
+			let notAd = false;
+			let done = false;
+      await document.body.addEventListener('DOMSubtreeModified', function() {
+        ifChange = true;
+      }, false);
+			await autoCheck();
+			done = true;
+      if (ifChange) {
+				await autoCheck();
+				done = true; 
+      }
+      const spans = document.getElementsByTagName('span');
+      for (let i = 0; i < spans.length; i++) {
+        if (spans[i].innerText.includes('광고성')) {
+          spans[i].click();
+          notAd = true;
+          break;
+        }
+      };
+      if (!notAd) {
+        const labels = document.getElementsByTagName('label');
+        for (let i = 0; i < labels.length; i++) {
+          if (labels[i].innerText.includes('광고성')) {
+            labels[i].click();
+            break;
+          }
+        };
+			}
+			if (!done) {
+				autoCheck();
+			}
+    }
+  } catch (e) {
+    console.error;
+  }
+})();
